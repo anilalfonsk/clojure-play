3a005f31da18ef7abaa769cae91d586fd29e7358
Anil Kodiyan <anil.kodiyan@anil.kodiyan>
2021-11-09 18:27:16 +0530

    Adding map

--- src/clojure_play/lesson5.clj
+++ src/clojure_play/lesson5.clj
@@ -28,3 +28,57 @@
 
 (println (update bucket :backpack #(- %1 1)))
 
+(println (dissoc bucket :backpack))
+
+(def order {:backpack {
+                       :quantity 2,
+                       :price 80
+                       },
+            :shirt {
+                    :quantity 2
+                    :price 40
+                    }})
+
+(println order)
+
+(assoc order :keychain {
+                         :quantity 1,
+                         :price 10
+                         })
+
+(def order (assoc order :keychain {
+                                   :quantity 1,
+                                   :price 10
+                                   }))
+
+(println order)
+
+(println (get order :backpack))
+(println (get order :cheer))
+(println (get order :cheer {}))
+
+; Preferred way to get data is as follows
+(println (:backpack order))
+(println (:cheer order {}))
+(println (:quantity (:backpack order)))
+(println (update-in order [:backpack :quantity] inc))
+
+;MOST USED AND PREFERRED WAY IS AS FOLLOWS
+;Its also treated as Threading
+(println (-> order
+             :backpack
+             :quantity))
+
+;this is called threading and is extensively used in the clojure coding
+(-> order :backpack :quantity println)
+
+(def clients {
+              :15 {
+                   :name "william"
+                   :certificates ["ONE" "TWO" "THREE" "FOUR"]
+                   }
+              })
+
+(println (count (-> clients :15 :certificates)))
+
+
