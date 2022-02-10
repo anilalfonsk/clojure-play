ca34c6a0ea34ad40d010a39d631b83a043fd893e
Anil Kodiyan <anil.kodiyan@anil.kodiyan>
2022-02-10 16:40:40 +0100

    Adding few more

--- src/store/lesson1.clj
+++ src/store/lesson1.clj
@@ -10,6 +10,37 @@
 '(1 2 3 4 5)
 
 ;Set
-#{1 2 3 1}
+#{1 2 3}
 
-(prinln )
+;to print all the elements in array.
+(map println ["daniel" "rapper" "mapper" "engineer"])
+
+(println (first ["daniel" "rapper" "mapper" "engineer"]))
+
+(println (rest ["daniel" "rapper" "mapper" "engineer"]))
+
+(println (next ["daniel" "rapper" "mapper" "engineer"]))
+
+(println (first []))
+
+(println (rest []))
+
+(println (next []))
+
+(defn my-map-v1
+  [function collection]
+  (function (first collection)))
+
+(my-map-v1 println ["daniel" "rapper" "mapper" "engineer"])
+
+(defn my-map-v2
+  [function collection]
+  (let [first-element (first collection)]
+    (if (not (nil? first-element))
+      (do
+        (function first-element)
+        (my-map-v2 function (rest collection))))))
+
+(my-map-v2 println ["daniel" "rapper" "mapper" "engineer"])
+
+(my-map-v2 println ["daniel" false "rapper" "mapper" "engineer"])
